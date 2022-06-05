8862efa59b5ee5081e6a9f78bc6fbe8dc5c1f9a4
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-06-05 23:34:06 +0300

    Initial commit

---
+++ toDoApp/Scene/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  toDoApp
+//
+//  Created by Akın Aksoy on 5.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var toDoTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        toDoTableView.delegate = self
+        toDoTableView.dataSource = self
+        
+        UINavigationBar.appearance().barTintColor = .green
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus" ), style: .done, target: self, action: nil)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        
+    }
+
+    
+    
+
+}
+
+extension ViewController : UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath) as! ToDoTableViewCell
+        cell.TimeLabel.text = "22.02.2021 - 14.30"
+        cell.titleLabel.text = "Go to Shopping"
+        cell.toDoCheckbox.image = UIImage(systemName: "checkmark.square")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 110
+    }
+    
+    
+    
+}
---
+++ toDoApp/Scene/ToDoTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoTableViewCell.swift
+//  toDoApp
+//
+//  Created by Akın Aksoy on 5.06.2022.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var toDoCheckbox: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var TimeLabel: UILabel!
+    
+    
+
+    
+}
