e55edc96cbfb168c2ab5b8ee85f290ffdab42ddf
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-06-07 23:42:58 +0300

    Description TextField is added

--- toDoApp/Scene/createToDoPage/createToDoViewController.swift
+++ toDoApp/Scene/createToDoPage/createToDoViewController.swift
@@ -10,27 +10,33 @@ import SnapKit
 class createToDoViewController: UIViewController {
 
     let titleArea = UIView()
-    let titleDescriptionLabel = Label.init().descriptionLabel
+    let titleHeaderLabel = Label.init().descriptionLabel
     let titleTextfield = TextField.init().textfield
     let selectDateTimeArea = UIView()
-    let dateDescriptionLabel = Label.init().descriptionLabel
+    let dateHeaderLabel = Label.init().descriptionLabel
     let datePicker = Datepicker.init().datePicker
-    
+    let descriptionArea = UIView()
+    let descriptionTextfield = TextField.init().textfield
+    let descriptionHeaderLabel = Label.init().descriptionLabel
     override func viewDidLoad() {
         super.viewDidLoad()
         makeDesign()
     }
     
     func makeDesign() {
-        titleDescriptionLabel.text = "Title"
-        dateDescriptionLabel.text = "Select Date"
+        titleHeaderLabel.text = "Title"
+        dateHeaderLabel.text = "Select Date"
+        descriptionHeaderLabel.text = "Description"
         view.backgroundColor = UIColor().setPurple2
         view.addSubview(titleArea)
         view.addSubview(titleTextfield)
-        view.addSubview(titleDescriptionLabel)
+        view.addSubview(titleHeaderLabel)
         view.addSubview(selectDateTimeArea)
         view.addSubview(datePicker)
-        view.addSubview(dateDescriptionLabel)
+        view.addSubview(dateHeaderLabel)
+        view.addSubview(descriptionArea)
+        view.addSubview(descriptionTextfield)
+        view.addSubview(descriptionHeaderLabel)
         makeConstraints()
     }
     
@@ -48,25 +54,41 @@ class createToDoViewController: UIViewController {
             make.centerX.equalTo(titleArea)
             make.centerY.equalTo(titleArea)
         }
-        titleDescriptionLabel.snp.makeConstraints { make in
+        titleHeaderLabel.snp.makeConstraints { make in
             make.bottom.equalTo(titleTextfield.snp_topMargin).offset(-8)
             make.left.equalTo(titleTextfield.snp_leftMargin).offset(8)
         }
         selectDateTimeArea.snp.makeConstraints { make in
-            make.top.equalTo(titleArea.snp_bottomMargin)
+            make.top.equalTo(titleArea.snp_bottomMargin).offset(16)
             make.left.equalToSuperview()
             make.height.equalToSuperview().multipliedBy(0.10)
             make.width.equalToSuperview().multipliedBy(0.92)
         }
         datePicker.snp.makeConstraints { make in
-            make.top.equalTo(selectDateTimeArea)
+            make.centerY.equalTo(selectDateTimeArea)
             make.left.equalTo(titleTextfield)
             make.width.equalTo(selectDateTimeArea).dividedBy(2).offset(5)
             make.height.equalTo(selectDateTimeArea).dividedBy(2)
         }
-        dateDescriptionLabel.snp.makeConstraints { make in
+        dateHeaderLabel.snp.makeConstraints { make in
             make.bottom.equalTo(datePicker.snp_topMargin).offset(-8)
             make.left.equalTo(datePicker.snp_leftMargin).offset(8)
+        }
+        descriptionArea.snp.makeConstraints { make in
+            make.height.equalToSuperview().multipliedBy(0.10)
+            make.width.equalToSuperview()
+            make.top.equalTo(selectDateTimeArea.snp_bottomMargin).offset(16)
+            make.left.equalToSuperview()
+        }
+        descriptionTextfield.snp.makeConstraints { make in
+            make.width.equalTo(descriptionArea).multipliedBy(0.92)
+            make.height.equalTo(descriptionArea).dividedBy(2)
+            make.centerX.equalTo(descriptionArea)
+            make.centerY.equalTo(descriptionArea)
+        }
+        descriptionHeaderLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(descriptionTextfield.snp_topMargin).offset(-8)
+            make.left.equalTo(descriptionTextfield.snp_leftMargin).offset(8)
         }
     }
 }
