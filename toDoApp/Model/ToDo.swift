edd01094207d096696d09c2c925b5a59d47a8514
Akin Aksoy <48281659+akinaksoy@users.noreply.github.com>
2022-06-09 22:24:53 +0300

    ToDo Model is created

---
+++ toDoApp/Model/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoApp
+//
+//  Created by Akın Aksoy on 9.06.2022.
+//
+
+import Foundation
+
+struct ToDo{
+    var title : String = ""
+    var description : String = ""
+    var date = Date()
+}
